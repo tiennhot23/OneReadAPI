db7c1e53562c6bc2bba9c4c71e5ae432e1723cc4
tiennhot23@gmail.com <tiennhot23@gmail.com>
2022-01-21 13:06:05 +0700

    fine

--- utils/utils.js
+++ utils/utils.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken')
 
 module.exports = {
     generateAccessToken: (user) => {
-        return jwt.sign({user}, process.env.ACCESSTOKEN, { expiresIn: '2s'})
+        return jwt.sign({user}, process.env.ACCESSTOKEN, { expiresIn: '365d'})
     },
     generateRefreshToken: (user) => {
         return jwt.sign({user}, process.env.REFRESHTOKEN)
